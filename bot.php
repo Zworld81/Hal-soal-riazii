ed2aa64bcba092bd507fd18aa67235a3efeb42ab
Milad Dehghan <milad.dehghan@hotmail.com>
2022-02-04 14:29:03 +0330

    bot messages

--- bot.php
+++ bot.php
@@ -1,9 +1,16 @@
 <?php
 $url = $_GET['url'];
 
+file_get_contents($url);
+return "ok";
+/*
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $last);
+fclose($myfile);
+
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $last);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
 // grab URL and pass it to the browser
@@ -11,3 +18,5 @@ curl_exec($ch);
 
 // close cURL resource, and free up system resources
 curl_close($ch);
+
+return "ok";*/
