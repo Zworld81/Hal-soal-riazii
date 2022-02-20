9fa0f0a46a78b9c3534db9effcb1c8ebc3e3b412
Milad Dehghan <milad.dehghan@hotmail.com>
2022-02-20 19:01:40 +0330

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,6 +20,7 @@ class UserSeeder extends Seeder
             'phone_number' => "09383663723",
             'level' => 0,
             'referral_code' => 'abcd',
+            'stars' => 100,
             'password' => Hash::make("12345678"),
         ]);
     }
